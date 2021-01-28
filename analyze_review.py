e9b927b80875286547340693e52320144a929769
yair-grinb <44864741+yair-grinb@users.noreply.github.com>
2021-01-28 08:54:21 +0200

    Update analyze_review.py

--- analyze_review.py
+++ analyze_review.py
@@ -2,38 +2,33 @@ import pandas as pd
 import itertools
 import operator
 
-inp_file = 'paper review.csv'
+inp_file = 'OSM_Paper_Review.csv'
 
 # read file and remove unincluded papers
 df = pd.read_csv(inp_file)
-removed = [3, 25, 47, 77, 106, 109, 139, 145, 147, 174, 177, 1003, 1004, 1006, 1011, 1022, 1031, 1033,
-           1040, 1041]
-df = df[~df.id.isin(removed)]
+
+columns = ["Authors’ disciplines", "Journal’s discipline", "Topics", 
+          "Authors' Geography (countries)", "Authors' Geography (continents)", "Study Area Geography (countries)", 
+          "Perspective on the community", "Evidence of engagement", "Geographic Correspondence"] 
 
 # harmonize the data - remove white spaces, new lines, etc. and transform strings to lists
-for c in ['Discipline 2 (updated)', 'Discipline journal 2 (updated)', 'Topic (updated)', 
-          'Geography (country)', 'Geography (continent)', 'Study Area Geography (country)', 
-          'Perspective on the community', 'Evidence of engagement']:
+for c in columns[:-1]:
     df[c] = df[c].str.replace('\r', '')
     df[c] = df[c].str.replace('\n', '')
     df[c] = df[c].str.strip()
     df[c] = df[c].str.split(',')
 
 # transfrom null values to string/lists of string
-df.loc[df['Study Area Geography (country)'].isnull(),['Study Area Geography (country)']] = df.loc[
-    df['Study Area Geography (country)'].isnull(),'Study Area Geography (country)'].apply(lambda x: ['NA'])
-df.loc[df['Geographic Correspondence'].isna(), ['Geographic Correspondence']] = 'NA'
+df.loc[df[columns[5]].isnull(),[columns[5]] = df.loc[
+    df[columns[5]].isnull(),columns[5]].apply(lambda x: ['NA'])
+df.loc[df[columns[-1]].isna(), [columns[-1]]] = 'NA'
 
 # remove whitespaces
-for c in ['Discipline 2 (updated)', 'Discipline journal 2 (updated)', 'Topic (updated)', 
-          'Geography (country)', 'Geography (continent)', 'Study Area Geography (country)', 
-          'Perspective on the community', 'Evidence of engagement']:
+for c in columns[:-1]:
     df[c] = df[c].map(lambda l:list(map(str.strip, l)))
         
 # print the values for each variable, to verify these are in order
-for c in ['Discipline 2 (updated)', 'Discipline journal 2 (updated)', 'Topic (updated)', 
-          'Geography (country)', 'Geography (continent)', 'Study Area Geography (country)', 
-          'Perspective on the community', 'Evidence of engagement']:
+for c in columns[:-1]:
     print(c)
     print(sorted(list(set(itertools.chain.from_iterable(df[c])))))
 
@@ -71,30 +66,28 @@ study_area_continent = {'Argentina': 'South America', 'Asia': 'Asia', 'Australia
 
 continent_rows = []
 for idx, row in df.iterrows():
-    continent_rows.append(list(set([study_area_continent[i] for i in row['Study Area Geography (country)']])))
+    continent_rows.append(list(set([study_area_continent[i] for i in row[columns[5]]])))
 df['Study Area Geography (continent)'] = continent_rows
 
 # create Table 3
 tab3 = pd.DataFrame(columns=['variable','value','papers','%'])
-for n in ['Discipline 2 (updated)', 'Discipline journal 2 (updated)', 'Topic (updated)', 
-          'Geography (continent)', 'Study Area Geography (continent)', 
-          'Perspective on the community', 'Evidence of engagement']:
+for n in [0, 1, 2, 4, 6, 7, 9]:
     counts = {}
     for idx, row in df.iterrows():
-        for i in row[n]:
+        for i in row[columns[n]]:
             if i not in counts:
                 counts[i] = 0
             counts[i] += 1
     for i in counts:
-        tab3 = tab3.append({'variable':n, 
+        tab3 = tab3.append({'variable':columns[n], 
                             'value':i, 
                             'papers': counts[i], 
                             '%': round(counts[i]*100/df.shape[0], 1)},
                            ignore_index=True)
 
-counts = df['Geographic Correspondence'].value_counts().to_dict()
+counts = df[columns[-2]].value_counts().to_dict()
 for i in counts:
-    tab3 = tab3.append({'variable':'Geographic Correspondence', 
+    tab3 = tab3.append({'variable':columns[-2], 
                         'value':i, 
                         'papers': counts[i], 
                         '%': round(counts[i]*100/df.shape[0], 1)},
@@ -104,18 +97,18 @@ tab3.to_csv('Table3.csv')
 # statistics for text
 # 1. number of papers by number of authors' disciplines
 for i in range(1, 5):
-    k = len([idx for idx, row in df.iterrows() if len(row['Discipline 2 (updated)'])==i])
+    k = len([idx for idx, row in df.iterrows() if len(row[columns[0]])==i])
     print('Authors from '+str(i)+' Disciplines:', k, round(k*100/df.shape[0], 1))
 
 # 2. number of papers by number of affiliated countries
 for i in range(1, 6):
-    k = len([idx for idx, row in df.iterrows() if len(row['Geography (country)'])==i])
+    k = len([idx for idx, row in df.iterrows() if len(row[columns[3]])==i])
     print('Authors from '+str(i)+' Countries:', k, round(k*100/df.shape[0], 1))
 
 # 3. number of papers by affiliated country
 country_counts = {}
 for idx, row in df.iterrows():
-    for i in set(row['Geography (country)']):
+    for i in set(row[columns[3]]):
         if i not in country_counts:
             country_counts[i] = 0
         country_counts[i] += 1
@@ -124,29 +117,29 @@ for k in sorted(country_counts.items(), key=operator.itemgetter(1), reverse=True
 
 # 4. number of papers by number of case study countries
 k = len([idx for idx, row in df.iterrows() 
-         if len(row['Study Area Geography (country)'])==1
-         and row['Study Area Geography (country)'] != ['Global']
-         and row['Study Area Geography (country)'] != ['NA']
-         and row['Study Area Geography (country)'] != ['Europe']
-         and row['Study Area Geography (country)'] != ['Asia']])
+         if len(row[columns[5]])==1
+         and row[columns[5]] != ['Global']
+         and row[columns[5]] != ['NA']
+         and row[columns[5]] != ['Europe']
+         and row[columns[5]] != ['Asia']])
 print('1 case study', k, round(k*100/df.shape[0], 1))
 for i in range(2, 7):
-    k = len([idx for idx, row in df.iterrows() if len(row['Study Area Geography (country)'])==i])
+    k = len([idx for idx, row in df.iterrows() if len(row[columns[5]])==i])
     print(str(i) + ' case studies:', k, round(k*100/df.shape[0], 1))
-k = len([idx for idx, row in df.iterrows() if len(row['Study Area Geography (country)'])>6])
+k = len([idx for idx, row in df.iterrows() if len(row[columns[5]])>6])
 print('more than 6 case studies:', k, round(k*100/df.shape[0], 1))
-print('maximal number of case studies:', max([len(row['Study Area Geography (country)']) 
+print('maximal number of case studies:', max([len(row[columns[5]]) 
                                               for idx, row in df.iterrows()]))
 
 # 5. number of papers using entire continent as a case study
 for cont in ['Europe', 'Asia']:
-    k = len([idx for idx, row in df.iterrows() if row['Study Area Geography (country)']==[cont]])
+    k = len([idx for idx, row in df.iterrows() if row[columns[5]]==[cont]])
     print('Paper using '+cont+' as case study', k, round(k*100/df.shape[0], 1))
 
 # 6. number of papers by case study country
 country_counts = {}
 for idx, row in df.iterrows():
-    for i in set(row['Study Area Geography (country)']):
+    for i in set(row[columns[5]]):
         if i not in country_counts:
             country_counts[i] = 0
         country_counts[i] += 1
@@ -154,24 +147,22 @@ for k in sorted(country_counts.items(), key=operator.itemgetter(1), reverse=True
     print(k[0], k[1], round(k[1]*100/df.shape[0], 1))
 
 # create files for producing Alluvial Diagrams
-names = ['Discipline 2 (updated)', 'Discipline journal 2 (updated)', 'Topic (updated)', 
-          'Geography (continent)', 'Study Area Geography (continent)', 
-          'Perspective on the community', 'Evidence of engagement']
-for c1 in range(len(names)-1):
-    for c2 in range(c1+1, len(names)):
+indices = [0, 1, 2, 6, 7]
+for c1 in range(len(indices)-1):
+    for c2 in range(c1+1, len(indices)):
         data = []
         for idx, row in df.iterrows():
-            for i in row[names[c1]]:
-                for j in row[names[c2]]:
+            for i in row[columns[indices[c1]]]:
+                for j in row[columns[indices[c2]]]:
                     data.append([i,j])
-        temp_df = pd.DataFrame(data, columns=[names[c1], names[c2]])
-        temp_df.to_csv('Sankey_'+names[c1]+'_'+names[c2]+'.csv')
+        temp_df = pd.DataFrame(data, columns=[columns[indices[c1]], columns[indices[c2]]])
+        temp_df.to_csv('Alluvial_'+columns[indices[c1]]+'_'+columns[indices[c2]]+'.csv')
 
 # create Alluvial diagrams files for the geographic correspondence variable
-for c2 in names:
+for c2 in indices:
     data = []
     for idx, row in df.iterrows():
-        for i in row[c2]:
-            data.append([row['Geographic Correspondence'], i])
-    temp_df = pd.DataFrame(data, columns=['Geographic Correspondence', c2])
-    temp_df.to_csv('Sankey_SameArea_'+c2+'.csv')
+        for i in row[columns[c2]]:
+            data.append([row[columns[-2]], i])
+    temp_df = pd.DataFrame(data, columns=[columns[-2], columns[c2]])
+    temp_df.to_csv('Alluvial_'+columns[-2]+'_'+columns[c2]+'.csv')
